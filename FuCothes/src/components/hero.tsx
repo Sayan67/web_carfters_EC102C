0132e1eb6f4192818dabfdc57d19444f6bf8ba22
Sayan67 <sayan.das.21@aot.edu.in>
2024-03-04 10:55:24 +0530

    Added picture and text in hero section

--- FuCothes/src/components/hero.tsx
+++ FuCothes/src/components/hero.tsx
@@ -3,12 +3,23 @@ import { Button } from "./ui/button";
 import { AiFillShopping } from "react-icons/ai";
 const Hero = () => {
   return (
-    <main className="w-full h-screen bg-slate-900/80 rounded-3xl">
-      <div>
-
+    <main className="w-full h-screen bg-slate-900/80 rounded-3xl py-6 px-6 flex">
+      <div className="relative w-full flex justify-end">
+        <h1 className="font-space_grotesk text-white text-[5vmax]">
+          Your Style Speaks
+        </h1>
       </div>
-      <div>
+      <div className="w-full flex items-center">
+        <Image
+        className="rounded-3xl"
+          src="/images/orangehero.jpeg"
+          alt="hero"
 
+          sizes="(min-width:1024px) 50vw, 100vw"
+          width={650}
+          height={900}
+          quality={100}
+        />
       </div>
     </main>
   );
